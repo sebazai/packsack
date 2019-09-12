52c882b8fcc57f6eb170fefffe6e8c344b7a2dda
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2019-09-12 18:15:21 +0300

    junit tests

---
+++ packsack/src/test/java/io/FileInputTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io;
+
+import java.io.FileNotFoundException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sebserge
+ */
+public class FileInputTest {
+    
+    
+    private FileInput stream;
+    
+    
+    @Before
+    public void setUp() {
+        String testfile = getClass().getClassLoader().getResource("streamtestfile").getPath();
+        stream = new FileInput(testfile);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
